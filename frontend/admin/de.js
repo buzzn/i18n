a0766c70fa2d8f3ac0ca541c8c4c18799723e92d
ChipJuggler <scsi.rdx@gmail.com>
2017-08-01 13:19:01 +0300

    powertaker view de translations

--- frontend/admin/de.js
+++ frontend/admin/de.js
@@ -1,7 +1,19 @@
 import meters from './de/meters';
 import registers from './de/registers';
+import contracts from './de/contracts';
+import persons from './de/persons';
+import tariffs from './de/tariffs';
+import payments from './de/payments';
+import organizations from './de/organizations';
+import addresses from './de/addresses';
 
 export default {
   ...meters,
   ...registers,
+  ...contracts,
+  ...persons,
+  ...tariffs,
+  ...payments,
+  ...organizations,
+  ...addresses,
 };
---
+++ frontend/admin/de/organizations.js
@@ -0,0 +1,9 @@
+export default {
+  'admin.organizations.headerOrganization': 'Organisation',
+  'admin.organizations.name': 'Name',
+  'admin.organizations.description': 'Beschreibung',
+  'admin.organizations.email': 'Email',
+  'admin.organizations.website': 'Webseite',
+  'admin.organizations.phone': 'Telefon',
+  'admin.organizations.fax': 'Fax',
+};
