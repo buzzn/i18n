fce1d4e6fb12ec210dadfe755e3c9bcbc4ffd538
Ralf Schroeder <ralf.schroeder@gmail.com>
2017-08-08 15:05:31 +0200

    translation for VM formulas

--- frontend/admin/de/formulas.js
+++ frontend/admin/de/formulas.js
@@ -1,4 +1,6 @@
 export default {
   'admin.formulas.headerFormulas': 'Formel',
   'admin.formulas.descriptionFormulas': 'Formelteile zur Berechnung des virtuellen Zählers.',
+  'admin.formulas.operator': 'Rechenzeichen',
+  'admin.formulas.registerName': 'Zählwerk',
 };
