ade68941f6cb1c29c08c24c6f20317dcd56b434c
Ralf Schroeder <ralf.schroeder@gmail.com>
2017-08-09 11:52:48 +0200

    DE trans for tariff labels

--- frontend/admin/de/tariffs.js
+++ frontend/admin/de/tariffs.js
@@ -1,4 +1,9 @@
 export default {
   'admin.tariffs.pricing': 'Preis',
-  'admin.tariffs.name': 'Preis',
+  'admin.tariffs.name': 'Tarif',
+  'admin.tariffs.beginDate': 'Beginnt am',
+  'admin.tariffs.endDate': 'Endet am',
+  'admin.tariffs.energyPricePerKw': 'Preis pro kWh',
+  'admin.tariffs.nbasePricePerMonth': 'Grundpreis pro Monat',
+  'admin.tariffs.headerTariffData': 'Tarifdaten',
 };
