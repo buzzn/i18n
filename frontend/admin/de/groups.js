9ed84665f08731b6c6ccb3e8012286d49ebf6b71
ChipJuggler <scsi.rdx@gmail.com>
2017-11-15 20:36:41 +0300

    groups

--- frontend/admin/de/groups.js
+++ frontend/admin/de/groups.js
@@ -1,4 +1,10 @@
 export default {
   'admin.groups.tableName': 'Gruppenname',
   'admin.groups.tableEnergyType': 'Quelle',
+  'admin.groups.headerSettings': 'Einstellungen',
+  'admin.groups.headerGroup': 'Energiegruppe',
+  'admin.groups.name': 'Gruppenname',
+  'admin.groups.energySources': 'Adresse',
+  'admin.groups.startDate': 'Starttermin',
+  'admin.addresses.address': 'Quellen',
 };
--- frontend/admin/en/groups.js
+++ frontend/admin/en/groups.js
@@ -1,4 +1,10 @@
 export default {
   'admin.groups.tableName': 'Group name',
   'admin.groups.tableEnergyType': 'Energy sources',
+  'admin.groups.headerSettings': 'Settings',
+  'admin.groups.headerGroup': 'Group',
+  'admin.groups.name': 'Name',
+  'admin.groups.energySources': 'Energy sources',
+  'admin.groups.startDate': 'Start date',
+  'admin.addresses.address': 'Address',
 };
