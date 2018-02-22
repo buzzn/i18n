a2d249e98704baef790d364600b6a4d80594af91
Ralf Schroeder <ralf.schroeder@gmail.com>
2018-02-22 08:34:32 +0100

    Malo translations

--- frontend/admin/de/market_locations.js
+++ frontend/admin/de/market_locations.js
@@ -1,7 +1,7 @@
 export default {
   'admin.marketLocations.navConsumption': 'Bezugsstellen',
   'admin.marketLocations.navProduction': 'Produktionsstellen',
-  'admin.marketLocations.navContracts': '?? Contracts',
-  'admin.marketLocations.navRegisters': '?? Registers',
+  'admin.marketLocations.navContracts': 'Vertäge',
+  'admin.marketLocations.navRegisters': 'Zählwerke',
   'admin.marketLocations.tableName': 'Name',
 };
