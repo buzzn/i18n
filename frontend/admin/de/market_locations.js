c8e988337b5fdbe28679c80224ea787e7683b829
Don Geolog <scsi.rdx@gmail.com>
2018-02-23 13:50:21 +0300

    Update market_locations.js

--- frontend/admin/de/market_locations.js
+++ frontend/admin/de/market_locations.js
@@ -1,7 +1,7 @@
 export default {
   'admin.marketLocations.navConsumption': 'Bezugsstellen',
   'admin.marketLocations.navProduction': 'Produktionsstellen',
-  'admin.marketLocations.navSystem': 'System',
+  'admin.marketLocations.navSystem': 'Systemzähler',
   'admin.marketLocations.navContracts': 'Verträge',
   'admin.marketLocations.navRegisters': 'Zählwerke',
   'admin.marketLocations.tableName': 'Name',
