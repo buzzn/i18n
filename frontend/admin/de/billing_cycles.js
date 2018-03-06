ac5aab066d812c946b1caf94ecf29b09965f6bf1
ChipJuggler <scsi.rdx@gmail.com>
2018-03-07 00:53:31 +0300

    bricks translations

--- frontend/admin/de/billing_cycles.js
+++ frontend/admin/de/billing_cycles.js
@@ -4,4 +4,11 @@ export default {
   'admin.billingCycles.modalHeaderAdd': 'Neue Turnusabrechnung',
   'admin.billingCycles.tableName': 'Name',
   'admin.billingCycles.tableDates': 'Zeitraum',
+  'admin.billingCycles.maloListTitle': 'Marktlokation',
+  'admin.billingCycles.bricksLegendTitle': 'Legende',
+  'admin.billingCycles.bricksLegendClosed': 'Abgerechnet',
+  'admin.billingCycles.bricksLegendOpen': 'Rechnung kalkul',
+  'admin.billingCycles.bricksLegendPT': 'SN Vertrag',
+  'admin.billingCycles.bricksLegendGAP': 'Lücken-vertrag',
+  'admin.billingCycles.bricksLegendGAP': 'Drittelbelieferte',
 };
--- frontend/admin/en/billing_cycles.js
+++ frontend/admin/en/billing_cycles.js
@@ -4,4 +4,11 @@ export default {
   'admin.billingCycles.modalHeaderAdd': 'New billing cycle',
   'admin.billingCycles.tableName': 'Name',
   'admin.billingCycles.tableDates': 'Dates',
+  'admin.billingCycles.maloListTitle': 'Market locations',
+  'admin.billingCycles.bricksLegendTitle': 'Legend',
+  'admin.billingCycles.bricksLegendClosed': 'Closed',
+  'admin.billingCycles.bricksLegendOpen': 'Open',
+  'admin.billingCycles.bricksLegendPT': 'Powertaker',
+  'admin.billingCycles.bricksLegendGAP': 'Gap',
+  'admin.billingCycles.bricksLegendGAP': 'Third party',
 };
