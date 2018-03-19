5fdcc59c8c00512c391865b994f29cc28bac1b1e
Ralf Schroeder <ralf.schroeder@gmail.com>
2018-03-19 09:50:23 +0100

    billing details EN labels

--- frontend/admin/en/billings.js
+++ frontend/admin/en/billings.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  'admin.billings.titleDetails': 'Invoice',
+  'admin.billings.customerName': 'Powertaker',
+  'admin.billings.dateRange': 'Billing period',
+  'admin.billings.customerEmail': 'Email',
+  'admin.billings.Status': 'Status',
+  'admin.billings.titleReference': 'Consumption amount',
+  'admin.billings.tableMeterSerial': 'Meter number',
+  'admin.billings.tableDates': 'Meter number',
+  'admin.billings.tableBeginReadingKwh': 'Begin reading',
+  'admin.billings.tableEndReadingKwh': 'End reading',
+  'admin.billings.tableConsumedEnergyKwh': 'Consumed energy',
+  'admin.billings.titleInvoices': 'Billing',  
+};
