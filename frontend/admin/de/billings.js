4444a30622ff1ec99ec330dc5a382ad47f813803
Ralf Schroeder <ralf.schroeder@gmail.com>
2018-03-19 09:52:24 +0100

    billing details DE labels

--- frontend/admin/de/billings.js
+++ frontend/admin/de/billings.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  'admin.billings.titleDetails': 'Rechnungsdaten',
+  'admin.billings.customerName': 'Stromnehmer',
+  'admin.billings.dateRange': 'Abrechnungszeitraum',
+  'admin.billings.customerEmail': 'Emailadresse',
+  'admin.billings.Status': 'Status',
+  'admin.billings.titleReference': 'Bezugsmenge',
+  'admin.billings.tableMeterSerial': 'Zählernummer',
+  'admin.billings.tableDates': 'Zeitraum',
+  'admin.billings.tableBeginReadingKwh': 'Zählerstand Beginn',
+  'admin.billings.tableEndReadingKwh': 'Zählerstand Ende',
+  'admin.billings.tableConsumedEnergyKwh': 'Bezugsmenge',
+  'admin.billings.titleInvoices': 'Abrechnung',  
+};
