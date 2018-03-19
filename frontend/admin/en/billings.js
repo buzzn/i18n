f9eff879377c2b4997865f6933bc2440b81b95f1
Don Geolog <scsi.rdx@gmail.com>
2018-03-19 12:27:02 +0300

    Update billings.js

--- frontend/admin/en/billings.js
+++ frontend/admin/en/billings.js
@@ -3,7 +3,7 @@ export default {
   'admin.billings.customerName': 'Powertaker',
   'admin.billings.dateRange': 'Billing period',
   'admin.billings.customerEmail': 'Email',
-  'admin.billings.Status': 'Status',
+  'admin.billings.status': 'Status',
   'admin.billings.titleReference': 'Consumption amount',
   'admin.billings.tableMeterSerial': 'Meter number',
   'admin.billings.tableDates': 'Meter number',
