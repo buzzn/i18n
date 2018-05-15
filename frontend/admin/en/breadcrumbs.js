7fb568aa80c9aa52932886225b7c4197ba8c2544
Don Geolog <scsi.rdx@gmail.com>
2018-05-15 11:25:09 +0200

    Update breadcrumbs.js

--- frontend/admin/en/breadcrumbs.js
+++ frontend/admin/en/breadcrumbs.js
@@ -2,4 +2,5 @@ export default {
   'admin.breadcumbs.myGroups': 'My Groups',
   'admin.breadcumbs.tariffs': 'Tariffs',
   'admin.breadcumbs.billingCycles': 'Billing Cycles',
+  'admin.breadcumbs.devices': 'Devices',
 };
