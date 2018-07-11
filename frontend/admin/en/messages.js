7328274645c18ded7f0c9802b62d2281a36a0b22
Don Geolog <scsi.rdx@gmail.com>
2018-07-11 13:23:24 +0300

    Update messages.js

--- frontend/admin/en/messages.js
+++ frontend/admin/en/messages.js
@@ -1,4 +1,3 @@
 export default {
   'admin.messages.cancelDirtyForm': 'You have unsaved changes, really close?',
-  'admin.messages.confirmDeleteGroup': 'Are you sure that you want to delete: ',
 };
