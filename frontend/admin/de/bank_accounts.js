aba1dea3df3cfd8d2109c283664cc2ba83d5152c
ChipJuggler <scsi.rdx@gmail.com>
2019-03-20 14:35:23 +0100

    bankAccounts

--- frontend/admin/de/bank_accounts.js
+++ frontend/admin/de/bank_accounts.js
@@ -3,4 +3,9 @@ export default {
   'admin.bankAccounts.tableBankName': 'Bank',
   'admin.bankAccounts.tableEncryptedIban': 'IBAN',
   'admin.bankAccounts.tableStatus': 'Status',
+  'admin.bankAccounts.holder': 'pleaseTranslate Holder',
+  'admin.bankAccounts.bic': 'BIC',
+  'admin.bankAccounts.iban': 'IBAN',
+  'admin.bankAccounts.bankName': 'pleaseTranslate Bank name',
+  'admin.bankAccounts.directDebit': 'pleaseTranslate Direct debit',
 };
--- frontend/admin/en/bank_accounts.js
+++ frontend/admin/en/bank_accounts.js
@@ -3,4 +3,9 @@ export default {
   'admin.bankAccounts.tableBankName': 'Bank',
   'admin.bankAccounts.tableEncryptedIban': 'IBAN',
   'admin.bankAccounts.tableStatus': 'Status',
+  'admin.bankAccounts.holder': 'Holder',
+  'admin.bankAccounts.bic': 'BIC',
+  'admin.bankAccounts.iban': 'IBAN',
+  'admin.bankAccounts.bankName': 'Bank name',
+  'admin.bankAccounts.directDebit': 'Direct debit',
 };
